099772280583d35a7621951b6a00211b3ea5b3de
aignacio <aignacio724@gmail.com>
2011-09-30 10:13:33 -0700

    Part A of project 2, image filtering

---
+++ imageFiltering.m
@@ -0,0 +1,9 @@
+%Project 2
+%Part A: Image Filtering
+
+a = imread('Boat2.tif');
+b = imread('building.gif');
+
+figure
+subplot(221), imshow(a), title('Boat');
+subplot(222), imshow(b), title('Building');
