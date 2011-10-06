ac1dc04ffe0be21ef1f81eaf5d47b9dff3dc2c91
aignacio724 <aignacio724@gmail.com>
2011-10-06 02:20:37 -0700

    Added all images to be tested

--- houghTrans.m
+++ houghTrans.m
@@ -1,10 +1,60 @@
+%Project 2
+%Part B, Hough Transform
+
 clear;
 clc;
-RGB = imread('corner_window.jpg');
 
-% Convert to intensity.
-I  = rgb2gray(RGB);
+a = rgb2gray(imread('corner_window.jpg'));
+b = rgb2gray(imread('corridor.jpg'));
+c = rgb2gray(imread('New York City.jpg'));
+d = rgb2gray(imread('tree.jpg'));
+
+windowSize = 5;
+sigma = 2;
 
 % Extract edges.
-BW = edge(I,'canny');
-[H,T,R] = hough(BW,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
+logA = laplacianOfGaussian(a, windowSize, sigma);
+logB = laplacianOfGaussian(b, windowSize, sigma);
+logC = laplacianOfGaussian(c, windowSize, sigma);
+logD = laplacianOfGaussian(d, windowSize, sigma);
+
+[HA,TA,RA] = hough(logA,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
+[HB,TB,RB] = hough(logB,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
+[HC,TC,RC] = hough(logC,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
+[HD,TD,RD] = hough(logD,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
+
+figure
+subplot(221), imshow(a), title('original');
+subplot(222), imshow(logA), title('LoG Edge Detect');
+subplot(2,2,[3 4]), imshow(imadjust(mat2gray(HA)),'XData',TA,'YData',RA,...
+      'InitialMagnification','fit');
+title('Hough Transform of corner window');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+
+figure
+subplot(221), imshow(b), title('original');
+subplot(222), imshow(logB), title('LoG Edge Detect');
+subplot(2,2,[3 4]), imshow(imadjust(mat2gray(HB)),'XData',TB,'YData',RB,...
+      'InitialMagnification','fit');
+title('Hough Transform of Corridor');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+
+figure
+subplot(221), imshow(c), title('original');
+subplot(222), imshow(logC), title('LoG Edge Detect');
+subplot(2,2,[3 4]), imshow(imadjust(mat2gray(HC)),'XData',TC,'YData',RC,...
+      'InitialMagnification','fit');
+title('Hough Transform of New York City');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+
+figure
+subplot(221), imshow(d), title('original');
+subplot(222), imshow(logD), title('LoG Edge Detect');
+subplot(2,2,[3 4]), imshow(imadjust(mat2gray(HD)),'XData',TD,'YData',RD,...
+      'InitialMagnification','fit');
+title('Hough Transform of Tree');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
